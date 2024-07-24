8553ec9edea7f8a9efa21f4788344b3aea961658
Cheryl Lee <yli.yanchen@gmail.com>
2024-07-23 22:22:10 -0700

    set error handler

---
+++ app/components/ErrorHandler.tsx
@@ -0,0 +1,14 @@
+// components/ErrorHandler.tsx
+import React from 'react';
+
+interface ErrorHandlerProps {
+  message: string | null;
+}
+
+const ErrorHandler: React.FC<ErrorHandlerProps> = ({ message }) => {
+  if (!message) return null;
+
+  return <div className='p-4 mb-4 text-red-500'>{message}</div>;
+};
+
+export default ErrorHandler;
